b6faa4e8ac6de322445685b2d50626c41d2e5dea
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-01-25 00:41:29 -0500

    mongoose and react init

--- AsianQuadPower/backend/controllers/goalControllers.js
+++ AsianQuadPower/backend/controllers/goalControllers.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler');
+
+const Goal = require('../model/goalModel')
 // @desc Get goals
 // @route Get /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
+    const goals = await Goal.find();
+    res.status(200).json(goals)
 })
 
 // @desc Seet goals
@@ -14,21 +17,40 @@ const setGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({ message: 'Set goals' })
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 // @desc Update goals
 // @route Put /api/goals
 // @access Private
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, res.body, {
+        new: true,
+    })
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goals
 // @route Delete /api/goals
 // @access Private
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {

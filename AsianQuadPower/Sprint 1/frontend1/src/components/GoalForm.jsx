37161bfb2d801cfe4221ff7f8c724c9d678e0053
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-01-29 21:30:05 -0500

    #3 #15

--- AsianQuadPower/Sprint 1/frontend1/src/components/GoalForm.jsx
+++ AsianQuadPower/Sprint 1/frontend1/src/components/GoalForm.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
+import { createGoal } from '../features/goals/goalSlice'
 function GoalForm() {
+    const [text, setText] = useState('')
+    const dispatch = useDispatch()
+    const onSubmit = e => {
+        e.preventDefault()
+        dispatch(createGoal({ text }))
+        setText('')
+    }
+
     return (
-        <div>GoalForm</div>
+        <section className='form'>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="text">
+                        Goal
+                    </label>
+                    <input type="text" name="text" id="text" value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+                <form className="form-group">
+                    <button className="btn btn-block">Submit</button>
+                </form>
+            </form>
+        </section>
     )
 }
 

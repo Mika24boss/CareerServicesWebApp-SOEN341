4efb065de30aa6265333b85bc269b020bc8c54cc
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-01-29 21:30:05 -0500

    #3 #15

--- AsianQuadPower/Sprint 1/server.js
+++ AsianQuadPower/Sprint 1/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./backend/middleware/errorMiddleware')
 const connectDB = require('./backend/config/db')
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 connectDB();
 
@@ -14,6 +15,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./backend/routes/goalRoutes'));
 app.use('/api/users', require('./backend/routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './frontend1/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, './', 'frontend1', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));

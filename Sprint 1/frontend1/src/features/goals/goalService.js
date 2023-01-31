53d4b888531ac161c62f2381302397f81363d8ae
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-01-31 12:17:40 -0500

    Update frontend1

--- Sprint 1/frontend1/src/features/goals/goalService.js
+++ Sprint 1/frontend1/src/features/goals/goalService.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios'
 
 const API_URL = '/api/goals/'
 
@@ -6,11 +6,12 @@ const API_URL = '/api/goals/'
 const createGoal = async (goalData, token) => {
     const config = {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
+            Authorization: `Bearer ${token}`,
+        },
     }
 
     const response = await axios.post(API_URL, goalData, config)
+
     return response.data
 }
 
@@ -18,11 +19,12 @@ const createGoal = async (goalData, token) => {
 const getGoals = async (token) => {
     const config = {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
+            Authorization: `Bearer ${token}`,
+        },
     }
 
     const response = await axios.get(API_URL, config)
+
     return response.data
 }
 
@@ -44,4 +46,5 @@ const goalService = {
     getGoals,
     deleteGoal,
 }
+
 export default goalService

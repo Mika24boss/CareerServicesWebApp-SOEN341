8c523622d77df77bee62ee75bdc18cba9ff30c72
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-02-11 11:32:52 -0500

    backend update

--- Sprint 1/backend/model/goalModel.js
+++ Sprint 1/backend/model/goalModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const goalSchema = mongoose.Schema({
     user: {
@@ -10,8 +11,22 @@ const goalSchema = mongoose.Schema({
         type: String,
         required: [true, 'Please add a text value'],
     },
+    title: {
+        type: String,
+        required: true
+    },
+    applied: {
+        type: Boolean,
+        default: false
+    }
+
 }, {
     timestamps: true,
 })
 
+goalSchema.plugin(AutoIncrement, {
+    inc_field: 'job',
+    id: 'jobNums',
+    start_seq: 500
+})
 module.exports = mongoose.model('Goal', goalSchema)
--- Sprint 1/backend/server.js
+++ Sprint 1/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
+const mongoose = require('mongoose')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 const path = require('path');

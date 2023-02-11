daea3bda13d6ee71ba7c1e1d5a2b151cbf38f847
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-02-11 14:53:20 -0500

    Update backend with job database

--- Sprint 1/backend/server.js
+++ Sprint 1/backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 

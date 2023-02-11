e8f2bd0729a812eb79bb9ae6db93e1b0203be020
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-02-11 14:53:20 -0500

    Update backend with job database

--- Sprint 1/backend/routes/goalRoutes.js
+++ Sprint 1/backend/routes/goalRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalControllers');
-const { protect } = require("../middleware/authMiddleware")
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/jobControllers');
+const { protect } = require("../middleware/authMiddleware");
+const verifyJWT = require('../middleware/verifyJWT');
+
+router.use(verifyJWT)
 router.route('/').get(protect, getGoals).post(protect, setGoals)
 router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals)
 

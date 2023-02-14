440d8d8c10aeb5a6bdd041e8ab4bddde59bdd006
Mika24boss <turbogoog@gmail.com>
2023-02-14 18:41:35 -0500

    Organized information on job posting pages and added 2 API scripts

--- Sprint 2/frontend/src/routes/postings/[jobID]/+page.svelte
+++ Sprint 2/frontend/src/routes/postings/[jobID]/+page.svelte
@@ -4,14 +4,43 @@
     //jobTitle, companyName, location, jobID, jobDesc
 </script>
 
-<h1>{data.jobTitle} (#{data.jobID})</h1>
-<h2>{data.companyName}</h2>
-<h3>{data.location}</h3>
-<div>
+<div class="title">
+    <div class="topInfo">
+        <h1>{data.jobTitle} (#{data.jobID})</h1>
+        <h2>{data.companyName}</h2>
+        <h3>{data.location}</h3>
+    </div>
+    <button class="apply">
+        <b>Apply</b>
+    </button>
+</div>
+
+<div class="desc">
     <p>
         {@html data.jobDesc}
     </p>
 </div>
 
 <style>
+
+    h2 {
+        line-height: 1em;
+    }
+
+    .title {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .apply {
+        width: 10em;
+        height: 5em;
+        font-size: 1.1em;
+        margin-top: 1.5em;
+    }
+
+    .desc {
+        outline: solid;
+        padding: 1em;
+    }
 </style>

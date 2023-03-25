a1a08c6084f97d1c61923f57cbb8a143e89aa066
jesseythach <jesseythach2002@gmail.com>
2023-03-24 21:22:28 -0400

    Completed the student dashboard #6 #29
    close #6
    close #29

--- Sprint 3/frontend/src/routes/dashboard_student/+page.svelte
+++ Sprint 3/frontend/src/routes/dashboard_student/+page.svelte
@@ -1,73 +1,65 @@
 <script>
-    import Interview from '$lib/components/Interview.svelte';
-    import authService from "$lib/features/authService.js";
-    import jobService from "$lib/features/jobService.js";
-    import {goto} from "$app/navigation";
-    import {onMount} from "svelte";
+	import Interview from '$lib/components/Interview.svelte';
+	import authService from '$lib/features/authService.js';
+	import jobService from '$lib/features/jobService.js';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-    let jobsPack = loadAllJobs();
-    let user;
+	let jobsPack;
+	let interviewsArray;
+	let user;
 
-    async function loadAllJobs() {
-        await onMount(() => {
-            user = authService.getUser();
-        })
+	onMount(() => {
+		user = authService.getUser();
+		jobsPack = loadAllJobs();
+	});
 
-        if (user === null || user === undefined) {
-            await goto('/');
-        }
+	async function loadAllJobs() {
+		if (user === null || user === undefined) {
+			await goto('/');
+		}
 
-        const jobs = await jobService.getJobs(user.token);
+		const jobs = await jobService.getJobs(user.token);
+		jobsPack = jobs.map(function(job) {
+			return {
+				jobID: job.jobID,
+				title: job.title,
+				companyName: job.companyName,
+				location: job.location,
+				isActive: job.isActive,
+				applicants: job.applicants
+			};
+		});
 
-        jobsPack = jobs.map(function (job) {
-            return {
-                jobID: job.jobID,
-                title: job.title,
-                companyName: job.companyName,
-                location: job.location,
-                isActive: job.isActive,
-                applicants: job.applicants,
-            }
-        });
-        console.log(jobsPack)
-    }
+		let status, studentID = '';
+		let appsArray;
+		interviewsArray = [];
 
-    async function loadDashboard(){
-      let status, studentID;
-      let appsArray;
-      console.log('before')
-      for(let i=0; i<jobsPack.length; i++){
-        console.log('in loop')
-        status = jobsPack[i].isActive;
-        appsArray = jobsPack[i].applicants
-        console.log('status: '+ status)
-        console.log('appsArray: '+ appsArray)
+		for (let i = 0; i < jobsPack.length; i++) {
+			status = jobsPack[i].isActive;
+			appsArray = jobsPack[i].applicants;
 
-        for(let j=0;j<appsArray.length;j++){
-          studentID = appsArray[j].id;
-        }/*
-        if((status === true) && (studentID === user.id)){
-          // then display in dashboard
-        }*/
-      }
-    }
-
+			for (let j = 0; j < appsArray.length; j++) {
+				studentID = appsArray[j];
+				if ((status === true) && (studentID === user._id)) interviewsArray.push(jobsPack[i]);
+			}
+		}
+	}
 </script>
 
-<div class='student-dashboard' on:load={loadDashboard()}>
-        <h1>Upcoming Interviews</h1>
 
-        {#await jobsPack}
-        {:then jobsPack}
+<div class='student-dashboard'>
+	<h1>Upcoming Interviews</h1>
 
-        <div class='interviews'>
-            {#each jobsPack as interview}
-                <Interview {...interview}/>
-            {/each}
-        </div>
+	{#if interviewsArray}
+		<div class='interviews'>
+			{#each interviewsArray as interview}
+				<Interview {...interview} />
+			{/each}
+		</div>
+	{/if}
+</div>
 
-    {/await}
-    </div>
 
 <style>
 
@@ -80,13 +72,12 @@
     }
 
     .interviews {
-				display: grid;
-				grid-template-columns: 2fr 2fr;
-				grid-gap: 1em;
-				justify-items: stretch;
-				position: relative;
-				left: 2em;
+        display: grid;
+        grid-template-columns: 2fr 2fr;
+        grid-gap: 1em;
+        justify-items: stretch;
+        position: relative;
+        left: 2em;
     }
-
 </style>
 

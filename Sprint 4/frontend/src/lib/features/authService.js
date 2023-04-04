266c3c1abfeb0b0be55816c051891911cf4b068b
anhtuann <n_anhtua@live.concordia.ca>
2023-04-03 21:34:13 -0400

    File handling in profile picture

--- Sprint 4/frontend/src/lib/features/authService.js
+++ Sprint 4/frontend/src/lib/features/authService.js
@@ -141,6 +141,7 @@ const deleteInterview = async (/** @type {string} */ applicantID, /** @type {any
     return {data: response.data, error: response.response?.status};
 }
 
+// Upload Profile Image
 const uploadProfileImage = async (/** @type {string} */ userData, /** @type {any} */ token) => {
     const config = {
         headers: {
@@ -148,7 +149,7 @@ const uploadProfileImage = async (/** @type {string} */ userData, /** @type {any
         },
     }
     try {
-        const response = await axios.patch(API_URL + '/uploadProfileImage', userData, config);
+        const response = await axios.patch(API_URL + 'uploadProfileImage', userData, config);
         return response.data;
     } catch (error) {
         console.log(error);
@@ -156,6 +157,7 @@ const uploadProfileImage = async (/** @type {string} */ userData, /** @type {any
     }
 }
 
+// Upload CV
 const uploadCV = async (/** @type {string} */ userData, /** @type {any} */ token) => {
     const config = {
         headers: {
@@ -163,7 +165,7 @@ const uploadCV = async (/** @type {string} */ userData, /** @type {any} */ token
         },
     }
     try {
-        const response = await axios.patch(API_URL + '/uploadResume', userData, config);
+        const response = await axios.patch(API_URL + 'uploadResume', userData, config);
         return response.data;
     } catch (error) {
         console.log(error);
--- Sprint 4/frontend/src/routes/profile/+page.svelte
+++ Sprint 4/frontend/src/routes/profile/+page.svelte
@@ -6,6 +6,7 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { authService } from '$lib/features/authService.js';
+	import { fileService } from '$lib/features/fileService.js';
 
 	let user;
 	let previewUrl;
@@ -15,18 +16,33 @@
 		const reader = new FileReader();
 		reader.onload = (e) => {
 			previewUrl = e.target.result;
+			console.log('Preview URL:', previewUrl);
 		};
 		reader.readAsDataURL(file);
 	}
 
-	onMount(() => {
-		const input = document.querySelector('#image-upload');
-		input.addEventListener('change', handleImageChange);
+	async function uploadPic(){
+		const uploadimage = document.getElementById('image')
+		console.log(uploadimage.files[0])
+		const formData = new FormData();
+		formData.append("id", user._id);
+		formData.append("name", uploadimage.files[0].name);
+		formData.append("profileImage", uploadimage.files[0]);
 
-		loadUser();
-		document.getElementById('name').value = user.name;
-		document.getElementById('email').value = user.email;
-	});
+		const response = await authService.uploadProfileImage(formData, user.token);
+		console.log(response)
+	}
+
+	async function uploadCV(){
+		const uploadfile = document.getElementById('cv')
+		const formData = new FormData();
+		formData.append("id", user._id);
+		formData.append("name", uploadfile.files[0].name);
+		formData.append("resume", uploadfile.files[0]);
+
+		const response = await authService.uploadCV(formData, user.token);
+		console.log(response)
+	}
 
 	async function loadUser() {
 		user = authService.getUser();
@@ -47,6 +63,31 @@
 		console.log(response);
 	}
 
+	onMount(() => {
+		loadUser();
+
+		const input = document.querySelector('#image');
+		input.addEventListener('change', handleImageChange);
+
+		const profilePic = fileService.getFileByID(user.profilePicture, user.token);
+		console.log(user.profilePicture)
+		console.log(profilePic)
+		//    const blob = new Blob([profilePic.data], { type: profilePic.contentType });
+		//    console.log(blob)
+		//     const reader = new FileReader();
+		//     reader.onload = (e) => {
+		//        previewUrl = e.target.result;
+		//     };
+		//    reader.readAsDataURL(profilePic.name);
+
+		document.getElementById('name').value = user.name;
+		document.getElementById('email').value = user.email;
+		document.getElementById('cv').file = fileService.getFileByID(user.resume, user.token);
+		document.getElementById('previewImage').src = profilePic;
+		// document.getElementById('previewImage').src = URL.createObjectURL(blob);
+		// document.getElementById('previewImage').src = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F12%2F16%2Fanime-cat-names-1204854078-2000.jpg"
+	});
+
 </script>
 
 <div class='profile-page'>
@@ -54,12 +95,12 @@
 
 	<h1 style='text-align: left;'>Profile</h1>
 	<div class='profile'>
-		<div class='profile-pic'>
+		<div class='avatar-upload'>
 			<h3>Avatar</h3>
 
 			<div class='image-container'>
-				<label id='image-btn' for='image-upload'>
-					<input type='file' name='image-upload' id='image-upload' style='display: none;' accept='image/*'>
+				<label id='image-btn' for='image'>
+					<input type='file' name='image' id='image' style='display: none;' accept='image/*'>
 					<img id='previewImage'
 							 src={previewUrl || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'}
 							 alt='Click to upload image'>
@@ -67,7 +108,7 @@
 			</div>
 
 			<div class='btn' style='padding-top: 10px;'>
-				<button>Change</button>
+				<button on:click={uploadPic}>Change</button>
 			</div>
 		</div>
 
@@ -87,15 +128,15 @@
 			</div>
 		</div>
 
-		<div class='resume'>
+		<div class='cv-upload'>
 			<h3>Resume</h3>
 
 			<div class='btn' style='text-align: left;'>
-				<input type='file' id='file-upload' name='file' accept='application/pdf,application/msword,.doc,docx'>
+				<input type='file' id='cv' name='cv' accept='application/pdf,application/msword,.doc,docx'>
 			</div>
 
 			<div class='btn'>
-				<input type='submit' value='Upload' style='cursor: pointer; width: auto; border-radius: 10px;'>
+				<input type='submit' value='Upload' on:click={uploadCV} style='cursor: pointer; width: auto; border-radius: 10px;'>
 			</div>
 		</div>
 	</div>
@@ -152,7 +193,7 @@
         margin: auto;
     }
 
-    .profile-pic {
+    .avatar-upload {
         background-color: #141414;
         display: flex;
         flex-direction: column;
@@ -202,7 +243,7 @@
         color: white;
     }
 
-    .resume {
+    .cv-upload {
         background-color: #141414;
         display: flex;
         flex-direction: column;
@@ -211,7 +252,7 @@
         padding: 10px 30px 10px;
     }
 
-    input[id=file-upload] {
+    input[id=cv] {
         border: none;
     }
 
